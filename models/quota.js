41d087baa2a40e6a7ef66eb9bb2c32466e87cb93
Kalana99 <kalanarajika99@gmail.com>
2022-09-19 22:55:06 +0530

    quota model created

---
+++ models/quota.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const quotaSchema = new Schema({
+    vehicleType: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    fuelType: {
+        type: String,
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    }
+}, { timestamps: true });
+
+const Quota = mongoose.model('Quota', quotaSchema);
+module.exports = Quota;
