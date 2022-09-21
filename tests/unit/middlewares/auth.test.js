65cac2308a1b2b19072901dfe7fb623c162d9702
Kalana99 <kalanarajika99@gmail.com>
2022-09-21 22:53:47 +0530

    middleware test files created

---
+++ tests/unit/middlewares/auth.test.js
@@ -0,0 +1,11 @@
+const {requireAuth, createToken} = require('../../../middleware/auth');
+
+describe("auth middleware check", () => {
+    
+    beforeAll(async () => {});
+
+    // it("should find a document matches to the regNo", async () => {
+    //     const vehicle = await findVehicleByRegNo('AAA-6756');
+    //     expect(vehicle).toEqual(null);
+    // });
+});
---
+++ tests/unit/middlewares/encryptionHandler.test.js
@@ -0,0 +1,11 @@
+const {checkEncryptedCredential, encryptCredential} = require('../../../middleware/encryptionHandler');
+
+describe("encryption middleware check", () => {
+    
+    beforeAll(async () => {});
+
+    // it("should find a document matches to the regNo", async () => {
+    //     const vehicle = await findVehicleByRegNo('AAA-6756');
+    //     expect(vehicle).toEqual(null);
+    // });
+});
