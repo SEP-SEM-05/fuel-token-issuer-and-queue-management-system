932b98eb0165be66ed428f4e7cb0264d8f449226
Kalana99 <kalanarajika99@gmail.com>
2022-09-25 18:34:09 +0530

    client authentication removed

--- controllers/orgController.js
+++ controllers/orgController.js
@@ -8,95 +8,6 @@ const vehicleDBHelper = require('../services/vehicleDBHelper');
 const auth = require('../middleware/auth');
 const encHandler = require('../middleware/encryptionHandler');
 
-//register an organization
-const register_post = async (req, res) => {
-
-    //registration process is different
-    //register all the vehicles under the organization to the system
-
-    let data = req.body;
-    let password = data.password;
-    let registrationNo = data.registrationNo;
-
-    data.password = await encHandler.encryptCredential(password);
-    let err = orgDBHelper.saveClient(data);
-
-    if(err){
-        let errField = (err.keyValue.email) ? 'email' : 'registrationNo';
-        res.status(400).json({
-            status: 'error',
-            error: errField + ' already exists!',
-        });
-    }
-    else{
-
-        let user = await orgDBHelper.findClientByregistrationNo(registrationNo);
-        let token = auth.createToken();
-        let name = user.name;
-
-        res.json({
-            status: 'ok',
-            token: token,
-            userType: 'organization',
-            data: {
-                registrationNo: registrationNo,
-                id: user._id,
-                name: name
-            }
-        });
-    }
-}
-
-//post organization login details
-const login_post = async (req, res) => {
-
-    const registrationNo = req.body.registrationNo;
-    const password = req.body.password;
-
-    try {
-
-        const user = await orgDBHelper.findClientByRegNo(registrationNo);
-
-        if(user !== null){
-            
-            let password_check = await encHandler.checkEncryptedCredential(password, user.password);
-
-            if(password_check){
-
-                let token = auth.createToken();
-                let name = user.name;
-
-                return_data = {
-                    status: 'ok',
-                    token: token,
-                    userType: 'organization',
-                    data: {
-                        registrationNo: registrationNo,
-                        id: user._id,
-                        name: name
-                    }
-                }
-                res.json(return_data);
-            }
-            else{
-                res.status(400).json({
-                    status: 'error',
-                    error: 'Authentication error!'
-                });
-            }
-        }
-        else{
-            res.status(400).json({
-                status: 'error',
-                error: 'Authentication error!'
-            });
-        }
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).json({});
-    }
-}
 
 //get organization dashboard info
 const get_dashboard = async (req, res) => {
@@ -131,7 +42,5 @@ const get_dashboard = async (req, res) => {
 }
 
 module.exports = {
-    register_post,
-    login_post,
     get_dashboard,
 }
--- controllers/personalController.js
+++ controllers/personalController.js
@@ -8,92 +8,6 @@ const vehicleDBHelper = require('../services/vehicleDBHelper');
 const auth = require('../middleware/auth');
 const encHandler = require('../middleware/encryptionHandler');
 
-//register a personal client
-const register_post = async (req, res) => {
-
-    let data = req.body;
-    let password = data.password;
-    let nic = data.nic;
-
-    data.password = await encHandler.encryptCredential(password);
-    let err = personalDBHelper.saveClient(data);
-
-    if(err){
-        let errField = (err.keyValue.email) ? 'email' : 'nic';
-        res.status(400).json({
-            status: 'error',
-            error: errField + ' already exists!',
-        });
-    }
-    else{
-
-        let user = await personalDBHelper.findClientByNic(nic);
-        let token = auth.createToken();
-        let fullName = user.firstName + " " + user.lastName;
-
-        res.json({
-            status: 'ok',
-            token: token,
-            userType: 'personal',
-            data: {
-                nic: nic,
-                id: user._id,
-                fullName: fullName
-            }
-        });
-    }
-}
-
-//post personal client login details
-const login_post = async (req, res) => {
-
-    const nic = req.body.nic;
-    const password = req.body.password;
-
-    try {
-
-        const user = await personalDBHelper.findClientByNic(nic);
-
-        if(user !== null){
-            
-            let password_check = await encHandler.checkEncryptedCredential(password, user.password);
-
-            if(password_check){
-
-                let token = auth.createToken();
-                let fullName = user.firstName + " " + user.lastName;
-
-                return_data = {
-                    status: 'ok',
-                    token: token,
-                    userType: 'personal',
-                    data: {
-                        nic: nic,
-                        id: user._id,
-                        fullName: fullName
-                    }
-                }
-                res.json(return_data);
-            }
-            else{
-                res.status(400).json({
-                    status: 'error',
-                    error: 'Authentication error!'
-                });
-            }
-        }
-        else{
-            res.status(400).json({
-                status: 'error',
-                error: 'Authentication error!'
-            });
-        }
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).json({});
-    }
-}
 
 //get personal client dashboard info
 const get_dashboard = async (req, res) => {
@@ -128,7 +42,5 @@ const get_dashboard = async (req, res) => {
 }
 
 module.exports = {
-    register_post,
-    login_post,
     get_dashboard,
 }
