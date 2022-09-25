c2b88733807e10623debe03b5fd19a7c82a27cb9
Kalana99 <kalanarajika99@gmail.com>
2022-09-25 18:35:06 +0530

    station dashboard method added

--- routes/stationRoutes.js
+++ routes/stationRoutes.js
@@ -4,5 +4,7 @@ const auth = require('../middleware/auth');
 
 const router = express.Router();
 
+//get dashboard info
+router.get('/dashboard/:id', auth.requireAuth, stationController.get_dashboard);
 
 module.exports = router;
