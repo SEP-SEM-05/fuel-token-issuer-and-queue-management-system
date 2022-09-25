1aaa9337384ad1ec22423ba4e8846dece7184b61
Kalana99 <kalanarajika99@gmail.com>
2022-09-25 18:33:18 +0530

    authentication removed

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -8,33 +8,8 @@ const admin_psw = process.env.ADMIN_PASSWORD
 const auth = require('../middleware/auth');
 const encHandler = require('../middleware/encryptionHandler');
 
-const login_post = async (req, res) => {
 
-    let username = req.body.username;
-    let password = req.body.password;
-
-    try {
-        if(username === admin_username && password === admin_psw){
-
-            let token = auth.createToken();
-
-            res.json({
-                status: 'ok',
-                token: token
-            });
-        }
-        else{
-            res.json({
-                status: 'error',
-                error: 'Authentication error!'
-            });
-        }
-    } 
-    catch (error) {
-        console.error(error);
-    }
-}
 
 module.exports = {
-    login_post,
+    
 }
