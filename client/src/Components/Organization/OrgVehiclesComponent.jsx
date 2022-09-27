e7e589f66682b34076f47943d4ee33d6d934bded
Dakshina Ranmal <76484102+dakshina99@users.noreply.github.com>
2022-09-27 15:32:52 +0530

    Ui fixed

--- client/src/Components/Organization/OrgVehiclesComponent.jsx
+++ client/src/Components/Organization/OrgVehiclesComponent.jsx
@@ -114,132 +114,18 @@ const top100Films = [
   ];
 
 const vehicles = [
-    ['ABC 1234', 'Diesel', '19/09/2022', 'Mortor Car', 34, 40, [{ title: '3 Idiots', year: 2009 }, { title: 'Snatch', year: 2000 }]],
-    ['ABD 4234', 'Petrol', '17/08/2022', 'Mortor Bike', 4, 5, [{ title: 'Snatch', year: 2000 }]],
-    ['ADT 4569', 'Petrol', '12/07/2022', 'Van', 30, 30, [{ title: 'Snatch', year: 2000 }]]
+    ['ABC 1234', 'Diesel', '19/09/2022', 'Mortor Car', 40, [{ title: '3 Idiots', year: 2009 }, { title: 'Snatch', year: 2000 }]],
+    ['ABD 4234', 'Petrol', '17/08/2022', 'Mortor Bike', 5, [{ title: 'Snatch', year: 2000 }]],
+    ['ADT 4569', 'Petrol', '17/08/2022', 'Van', 30, [{ title: 'Snatch', year: 2000 }]]
 ]
 
 
 
 
 export default function OrgVehicles() {
-    const [open, setOpen] = React.useState(false);
-    const [openSt, setOpenSt] = React.useState(false);
-    const [vehicle, setVehicle] = React.useState([]);
-    const [selectedStation, setSelectedStation] = React.useState('');
-    const [selectedStations, setSelectedStations] = React.useState([]);
-    const fixedOptions = vehicle[6] ? vehicle[6]: [];
-    const [value, setValue] = React.useState([...fixedOptions]);
-    console.log(fixedOptions);
-
-    const handleClickOpen = (vehicle) => {
-        setOpen(true);
-        setVehicle(vehicle);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    };
-
-    const handleClickOpenSt = (vehicle) => {
-        setOpenSt(true);
-        setVehicle(vehicle);
-    };
-
-    const handleCloseSt = () => {
-        setOpenSt(false);
-    };
-
-    const addStation = (newValue) => {
-        // if (newValue !== null && selectedStations.indexOf(newValue) === -1 && selectedStations.length < 3) {
-        //     setSelectedStations(...selectedStations.concat([newValue]));
-        // }
-        setSelectedStations(newValue)
-        console.log(selectedStation);
-
-    }
-
-    const removeSelectedStation = (st) => {
-        selectedStations.splice(selectedStations.indexOf(st), 1);
-
-        console.log(selectedStations);
-
-    }
-
 
     return (
         <Box>
-            <Dialog open={open} onClose={handleClose}  >
-
-                <DialogTitle sx={{ fontWeight: 'bold', pb: 1 }} >
-                    <Box sx={{ display: 'flex', alignItems: 'center' }}><PublishedWithChangesIcon sx={{ pr: 1 }} /> Request Fuel</Box>
-                </DialogTitle>
-                <Divider />
-
-                <DialogContent sx={{ pr: 8, pl: 4 }}>
-                    <Typography variant='h6'>
-                        <strong>{vehicle[0]}</strong> <Typography variant='caption'>{vehicle[3]}</Typography>
-                    </Typography>
-                    <Typography variant='subtitle1' sx={{ fontWeight: 'bold' }} >
-                        <Chip label={vehicle[1]} color={vehicle[1] === 'Diesel' ? 'success' : 'warning'} />
-                    </Typography>
-
-
-                    <Typography variant='h6' sx={{ pt: 4 }}>
-                        <strong>{vehicle[4]}</strong> liters remaining
-                    </Typography>
-                </DialogContent>
-                <DialogActions sx={{ pr: 3, pl: 3, pb: 3 }}>
-                    <Button variant='outlined' color={vehicle[1] === 'Diesel' ? 'success' : 'warning'} sx={{ width: "100%" }} >Request</Button>
-                </DialogActions>
-
-
-            </Dialog>
-
-            <Dialog open={openSt} onClose={handleCloseSt}  >
-
-                <DialogTitle sx={{ fontWeight: 'bold', pb: 1 }} >
-                    <Box sx={{ display: 'flex', alignItems: 'center' }}><PublishedWithChangesIcon sx={{ pr: 1 }} /> Selected Fuel Stations</Box>
-                </DialogTitle>
-                <Divider />
-
-                <DialogContent sx={{ pr: 8, pl: 4 }}>
-
-                    <Autocomplete
-                        multiple
-                        id="fixed-tags-demo"
-                        value={value}
-                        onChange={(event, newValue) => {
-                            value.length < 3 &&
-                                setValue([
-                                    ...newValue.filter((option) => fixedOptions.indexOf(option) === -1),
-                                ]);
-                        }}
-                        options={top100Films}
-                        getOptionLabel={(option) => option.title}
-                        renderTags={(tagValue, getTagProps) =>
-                            tagValue.map((option, index) => (
-                                <Chip
-                                    label={option.title}
-                                    {...getTagProps({ index })}
-                                />
-                            ))
-                        }
-                        style={{ width: 500 }}
-                        renderInput={(params) => (
-                            <TextField {...params} label="Fixed tag" variant="outlined" placeholder="Favorites" />
-                        )}
-                    />
-
-
-
-                </DialogContent>
-                <DialogActions sx={{ pr: 3, pl: 3, pb: 3 }}>
-                    <Button variant='outlined' color={vehicle[1] === 'Diesel' ? 'success' : 'warning'} sx={{ width: "100%" }} >Change Stations</Button>
-                </DialogActions>
-            </Dialog>
-
-
             <Grid item xs={12} sx={{ pl: { xs: "unset", lg: 3 }, my: -3 }} ><h1>Own Vehicles</h1></Grid>
             <Grid container spacing={8} justifyContent="center">
                 {vehicles.map((vehicle) => (
@@ -258,13 +144,13 @@ export default function OrgVehicles() {
                                 <Box sx={{ alignItems: 'center', display: "flex", pb: 1 }}>
                                     <OpacityIcon sx={{ pr: '5px' }} />
                                     <Typography variant="h6" display="block" >
-                                        {vehicle[4] === vehicle[5] ? <strong>{vehicle[5]}</strong> : <><strong>{vehicle[4]}</strong> out of <strong>{vehicle[5]}</strong></>} liters remaining
+                                        Weekly quota : <strong>{vehicle[4]}</strong> liters
                                     </Typography>
                                 </Box>
-                                <Button onClick={() => handleClickOpenSt(vehicle)} variant='contained' size='small' color='error' sx={{ fontWeight: 'bold' }}>Fuel Stations</Button>
+                               
                             </CardContent>
                             <CardActions sx={{ display: 'flex', alignItems: 'stretch', justifyContent: { xs: 'center', md: 'unset' } }}>
-                                <Button onClick={() => handleClickOpen(vehicle)} variant='contained' color='info'>
+                                <Button variant='contained' color='info'>
                                     <Typography variant='h6' sx={{ fontWeight: 'bold' }}>Request<br />Fuel</Typography>
                                 </Button>
                             </CardActions>
--- client/src/Components/Organization/RequestFuelOrg.jsx
+++ client/src/Components/Organization/RequestFuelOrg.jsx
@@ -1,23 +1,120 @@
 import React, { useState } from 'react'
-import { Box, Button, Card, CardActions, CardContent, Chip, Divider, Grid, InputAdornment, TextField, Typography } from '@mui/material'
+import { Autocomplete, Box, Button, Card, CardActions, CardContent, Chip, Dialog, DialogActions, DialogContent, DialogTitle, Divider, FormControl, Grid, InputAdornment, InputLabel, MenuItem, Select, TextField, Typography } from '@mui/material'
 import EventRepeatIcon from '@mui/icons-material/EventRepeat';
 import DirectionsCarIcon from '@mui/icons-material/DirectionsCar';
 
+const stationNameandCity = [
+    "station 01", "station 02", "station 03", "station 04"
+  ];
+
 const RequestFuelOrg = () => {
+    const [open, setOpen] = React.useState(false);
+    const [openSt, setOpenSt] = React.useState(false);
+    const [fuelType, setFuelType] = React.useState('');
+    const [value, setValue] = React.useState();
     const [dieselQuota] = useState([99, 99]);
     const [petrolQuota] = useState([89, 70]);
     const [lastDate] = useState("29/09/2022");
     const [vehicleCount] = useState(12);
+    const [stations] = useState(["station 01", "station 02", "station 03"])
+
+    const handleChange = (event) => {
+        setFuelType(event.target.value);
+    };
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleClickOpenSt = () => {
+        setOpenSt(true);
+        setValue(stations)
+    };
+
+    const handleCloseSt = () => {
+        setOpenSt(false);
+    };
 
     return (
         <Box>
-            <Grid item xs={12} sx={{ pl: { xs: "unset", lg: 3 }, my: -3 }} ><h1>Own Vehicles</h1></Grid>
-            <Grid container spacing={8} justifyContent="center">
-                <Grid item xs={12} md={9} sx={{ ml: 10 }}>
+            <Dialog open={open} onClose={handleClose}  >
+
+                <DialogTitle sx={{ fontWeight: 'bold', pb: 1 }} >
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}> Request Fuel</Box>
+                </DialogTitle>
+                <Divider />
+
+                <DialogContent sx={{ pr: 8, pl: 4 }}>
+                    <FormControl fullWidth>
+                        <InputLabel id="demo-simple-select-label">Fuel Type</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={fuelType}
+                            label="FuelType"
+                            onChange={handleChange}
+                        >
+                            <MenuItem value={10}>Auto Diesel</MenuItem>
+                            <MenuItem value={20}>Super Diesel</MenuItem>
+                            <MenuItem value={30}>Petrol 92 Octane</MenuItem>
+                            <MenuItem value={40}>Petrol 95 Octane</MenuItem>
+                        </Select>
+                    </FormControl>
+                </DialogContent>
+                <DialogActions sx={{ pr: 3, pl: 3, pb: 3 }}>
+                    <Button variant='outlined' color={'info'} sx={{ width: "100%" }} >Confirm and Request</Button>
+                </DialogActions>
+
+
+            </Dialog>
+            <Dialog open={openSt} onClose={handleCloseSt}  >
+
+                <DialogTitle sx={{ fontWeight: 'bold', pb: 1 }} >
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>Selected Fuel Stations</Box>
+                </DialogTitle>
+                <Divider />
+
+                <DialogContent sx={{ pr: 8, pl: 4, pt: 3 }}>
+
+                    <Autocomplete
+                        multiple
+                        id="fixed-tags-demo"
+                        value={value}
+                        onChange={(event, newValue) => {
+                            newValue.length < 4 && setValue(newValue);
+                        }}
+                        options={stationNameandCity}
+                        renderTags={(tagValue, getTagProps) =>
+                            tagValue.map((option, index) => (
+                                <Chip
+                                    sx={{ fontWeight: 'bold' }}
+                                    label={option}
+                                    {...getTagProps({ index })}
+                                />
+                            ))
+                        }
+                        sx={{ minWidth: { md: 500, xs: 'unset' } }}
+                        renderInput={(params) => (
+                            <TextField {...params} label="Fuel Stations" variant='outlined' color='info' focused />
+                        )}
+                    />
+                </DialogContent>
+                <DialogActions sx={{ p: 3 }}>
+                    <Button variant='outlined' color={'success'} sx={{ width: "100%" }} >Save Changes</Button>
+                </DialogActions>
+            </Dialog>
+
+            <Grid item xs={12} sx={{ pl: { xs: "unset", lg: 3 }, my: -3 }} ><h1>Home</h1></Grid>
+            <Grid container spacing={20} justifyContent="center">
+                <Grid item xs={12} md={8} sx={{ ml: 10 }}>
                     <form>
-                        <Card variant="outlined" sx={{ p: 1, boxShadow: 3, borderRadius: 3, display: 'flex', width: '75%', justifyContent: 'space-between', flexDirection: { xs: 'column', md: 'row' }, backgroundColor: '#282835', color: 'white', }}>
-                            <CardActions sx={{ display: 'flex', alignItems: 'stretch', justifyContent: { xs: 'center', md: 'unset' }, }}>
-                                <Button variant='contained' color='error'>
+                        <Card variant="outlined" sx={{ p: 1, boxShadow: 3, borderRadius: 3, display: 'flex', justifyContent: 'space-between', flexDirection: { xs: 'column', md: 'row' }, backgroundColor: '#282835', color: 'white', }}>
+                            <CardActions sx={{ display: 'flex', alignItems: 'stretch', justifyContent: { xs: 'center', lg: 'unset' }, }}>
+                                <Button onClick={handleClickOpenSt} variant='contained' color='error'>
                                     <Typography variant='h6' sx={{ fontWeight: 'bold' }}>Fuel<br />Stations</Typography>
                                 </Button>
                             </CardActions>
@@ -25,7 +122,7 @@ const RequestFuelOrg = () => {
                                 <Typography variant="h4" component="div" sx={{ fontWeight: 'bold' }}>
                                     Weekly Quota
                                 </Typography>
-                                <Grid item xs={12} sx={{ display: 'flex', flexDirection: { xs: 'column', md: 'row' }, }} >
+                                <Grid item xs={12} sx={{ display: 'flex', flexDirection: { xs: 'column', lg: 'row' }, }} >
                                     <Typography variant="h5" component="div" sx={{ fontWeight: 'bold', pt: 1, pr: 1, pb: 1 }}>
                                         <Chip color='success' label={`DIESEL ${dieselQuota[0] === dieselQuota[1] ? '' : `${dieselQuota[1]} /`} ${dieselQuota[0]} L REMAINING`} />
                                     </Typography>
@@ -33,19 +130,19 @@ const RequestFuelOrg = () => {
                                         <Chip color='warning' label={`PETROL ${petrolQuota[0] === petrolQuota[1] ? '' : `${petrolQuota[1]} /`} ${petrolQuota[0]} L REMAINING`} />
                                     </Typography>
                                 </Grid>
-                                <Box sx={{ justifyContent:'center', alignItems: 'center', display: "flex", pb: 1 }}>
+                                <Box sx={{ justifyContent: 'center', alignItems: 'center', display: "flex", pb: 1 }}>
                                     <Typography variant="h6" component="div" sx={{ pt: 8 }}>
                                         <DirectionsCarIcon /> Number of vehicles : {vehicleCount}
                                     </Typography>
                                 </Box>
-                                <Box sx={{ justifyContent:'center', alignItems: 'center', display: "flex", pb: 1 }}>
+                                <Box sx={{ justifyContent: 'center', alignItems: 'center', display: "flex", pb: 1 }}>
                                     <Typography variant="h6" component="div" sx={{}}>
                                         <EventRepeatIcon /> Last Taken Date : {lastDate}
                                     </Typography>
                                 </Box>
                             </CardContent>
-                            <CardActions sx={{ display: 'flex', alignItems: 'stretch', justifyContent: { xs: 'center', md: 'unset' } }}>
-                                <Button variant='contained' color='success'>
+                            <CardActions sx={{ display: 'flex', alignItems: 'stretch', justifyContent: { xs: 'center', lg: 'unset' } }}>
+                                <Button onClick={handleClickOpen} variant='contained' color='success'>
                                     <Typography variant='h6' sx={{ fontWeight: 'bold' }}>Request<br />Fuel</Typography>
                                 </Button>
                             </CardActions>
