a2a53fccc73b4f3f6f06015cf8dbf2b4e4ff82c4
Kalana99 <kalanarajika99@gmail.com>
2022-10-04 11:35:36 +0530

    token authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,283 +12,333 @@ const encHandler = require("../middleware/encryptionHandler");
 
 //register a personal client
 const register_post_personal = async (req, res) => {
-  let data = req.body;
-  let password = data.password;
-  let nic = data.nic;
 
-  try {
-    data.password = await encHandler.encryptCredential(password);
+    let data = req.body;
+    let password = data.password;
+    let nic = data.nic;
 
-    await personalDBHelper.saveClient(data);
+    try {
+        data.password = await encHandler.encryptCredential(password);
 
-    let user = await personalDBHelper.findClientByNic(nic);
+        await personalDBHelper.saveClient(data);
 
-    let token = auth.createToken();
-    let fullName = user.firstName + " " + user.lastName;
+        let user = await personalDBHelper.findClientByNic(nic);
 
-    res.json({
-      status: "ok",
-      token: token,
-      userType: "personal",
-      data: {
-        nic: nic,
-        id: user._id,
-        fullName: fullName,
-      },
-    });
-  } catch (err) {
-    console.log(err);
+        let token = auth.createToken();
+        let fullName = user.firstName + " " + user.lastName;
 
-    let errField = err.keyValue.email ? "email" : "nic";
-    res.status(400).json({
-      status: "error",
-      error: errField + " already exists!",
-    });
-  }
+        res.json({
+            status: "ok",
+            token: token,
+            userType: "personal",
+            data: {
+                nic: nic,
+                id: user._id,
+                fullName: fullName,
+            },
+        });
+    } catch (err) {
+        console.log(err);
+
+        let errField = err.keyValue.email ? "email" : "nic";
+        res.status(400).json({
+            status: "error",
+            error: errField + " already exists!",
+        });
+    }
 };
 
 //register an organization and register all the vehicles belongs to it
 const register_post_org = async (req, res) => {
-  let data = req.body;
-  let password = data.password;
-  let registrationNo = data.registrationNo;
 
-  try {
-    let orgClient = await orgDBHelper.findClientByRegNo(registrationNo);
+    let data = req.body;
+    let password = data.password;
+    let registrationNo = data.registrationNo;
 
-    if (!orgClient) {
-      res.status(400).json({
-        status: "error",
-        error: "Invalid Registration No.!",
-      });
-    } else if (orgClient.isRegistered) {
-      res.status(400).json({
-        status: "error",
-        error: "Organization is already registered!",
-      });
-    } else if (orgClient.email === data.email) {
-      res.status(400).json({
-        status: "error",
-        error: "email already exists!",
-      });
-    } else {
-      data.isRegistered = true;
-      data.password = await encHandler.encryptCredential(password);
+    try {
+        let orgClient = await orgDBHelper.findClientByRegNo(registrationNo);
 
-      await orgDBHelper.saveClient(registrationNo, data);
-      await vehicleDBHelper.registerAll(orgClient.vehicles);
+        if (!orgClient) {
+            res.status(400).json({
+                status: "error",
+                error: "Invalid Registration No.!",
+            });
+        } else if (orgClient.isRegistered) {
+            res.status(400).json({
+                status: "error",
+                error: "Organization is already registered!",
+            });
+        } else if (orgClient.email === data.email) {
+            res.status(400).json({
+                status: "error",
+                error: "email already exists!",
+            });
+        } else {
+            data.isRegistered = true;
+            data.password = await encHandler.encryptCredential(password);
 
-      let token = auth.createToken();
-      let name = data.name;
+            await orgDBHelper.saveClient(registrationNo, data);
+            await vehicleDBHelper.registerAll(orgClient.vehicles);
 
-      res.json({
-        status: "ok",
-        token: token,
-        userType: "organization",
-        data: {
-          registrationNo: registrationNo,
-          id: orgClient._id,
-          name: name,
-        },
-      });
+            let token = auth.createToken();
+            let name = data.name;
+
+            res.json({
+                status: "ok",
+                token: token,
+                userType: "organization",
+                data: {
+                    registrationNo: registrationNo,
+                    id: orgClient._id,
+                    name: name,
+                },
+            });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            status: "error",
+            error: "Internal server error!",
+        });
     }
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      status: "error",
-      error: "Internal server error!",
-    });
-  }
 };
 
 //admin login
 const login_post_admin = async (req, res) => {
-  let username = req.body.username;
-  let password = req.body.password;
 
-  const admin_username = process.env.ADMIN_USERNAME;
-  const admin_psw = process.env.ADMIN_PASSWORD;
+    let username = req.body.username;
+    let password = req.body.password;
 
-  try {
-    if (username === admin_username && password === admin_psw) {
-      let token = auth.createToken();
+    const admin_username = process.env.ADMIN_USERNAME;
+    const admin_psw = process.env.ADMIN_PASSWORD;
 
-      res.json({
-        status: "ok",
-        token: token,
-        userType: "admin",
-      });
-    } else {
-      res.status(400).json({
-        status: "error",
-        error: "Authentication error!",
-      });
+    try {
+        if (username === admin_username && password === admin_psw) {
+
+            let token_data = { userType: 'admin', username };
+
+            let accessToken = auth.createAccessToken(token_data);
+            let refreshToken = auth.createRefreshToken(token_data);
+
+            res.header("x-access-token", accessToken);
+            res.header("x-refresh-token", refreshToken);
+
+            res.json({
+                status: "ok",
+                userType: "admin",
+            });
+        } else {
+            res.status(400).json({
+                status: "error",
+                error: "Authentication error!",
+            });
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            status: "error",
+            error: "Internal server error!",
+        });
     }
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({
-      status: "error",
-      error: "Internal server error!",
-    });
-  }
 };
 
 //post personal client login details
 const login_post_personal = async (req, res) => {
 
-  const nic = req.body.nic;
-  const password = req.body.password;
+    const nic = req.body.nic;
+    const password = req.body.password;
 
-  try {
-    const user = await personalDBHelper.findClientByNic(nic);
+    try {
+        const user = await personalDBHelper.findClientByNic(nic);
 
-    if (user !== null) {
-      let password_check = await encHandler.checkEncryptedCredential(
-        password,
-        user.password
-      );
+        if (user !== null) {
+            let password_check = await encHandler.checkEncryptedCredential(
+                password,
+                user.password
+            );
 
-      if (password_check) {
-        let token = auth.createToken();
-        let fullName = user.firstName + " " + user.lastName;
+            if (password_check) {
 
-        return_data = {
-          status: "ok",
-          token: token,
-          userType: "personal",
-          data: {
-            nic: nic,
-            id: user._id,
-            fullName: fullName,
-          },
-        };
-        res.json(return_data);
-      } else {
-        res.status(400).json({
-          status: "error",
-          error: "Authentication error!",
+                let token_data = {
+                    userType: 'personal',
+                    id: user._id,
+                    nic
+                };
+
+                let accessToken = auth.createAccessToken(token_data);
+                let refreshToken = auth.createRefreshToken(token_data);
+
+                await personalDBHelper.saveRefreshToken(refreshToken, user._id);
+
+                res.header("x-access-token", accessToken);
+                res.header("x-refresh-token", refreshToken);
+
+                let fullName = user.firstName + " " + user.lastName;
+
+                return_data = {
+                    status: "ok",
+                    userType: "personal",
+                    data: {
+                        nic: nic,
+                        id: user._id,
+                        fullName: fullName,
+                    },
+                };
+                res.json(return_data);
+            } else {
+                res.status(400).json({
+                    status: "error",
+                    error: "Authentication error!",
+                });
+            }
+        } else {
+            res.status(400).json({
+                status: "error",
+                error: "Authentication error!",
+            });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            status: "error",
+            error: "Internal server error!",
         });
-      }
-    } else {
-      res.status(400).json({
-        status: "error",
-        error: "Authentication error!",
-      });
     }
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      status: "error",
-      error: "Internal server error!",
-    });
-  }
 };
 
 //post organization login details
 const login_post_org = async (req, res) => {
-  const registrationNo = req.body.registrationNo;
-  const password = req.body.password;
 
-  try {
-    const user = await orgDBHelper.findClientByRegNo(registrationNo);
+    const registrationNo = req.body.registrationNo;
+    const password = req.body.password;
 
-    if (user !== null) {
-      let password_check = await encHandler.checkEncryptedCredential(
-        password,
-        user.password
-      );
+    try {
+        const user = await orgDBHelper.findClientByRegNo(registrationNo);
 
-      if (password_check) {
-        let token = auth.createToken();
-        let name = user.name;
+        if (user !== null) {
+            let password_check = await encHandler.checkEncryptedCredential(
+                password,
+                user.password
+            );
 
-        return_data = {
-          status: "ok",
-          token: token,
-          userType: "organization",
-          data: {
-            registrationNo: registrationNo,
-            id: user._id,
-            name: name,
-          },
-        };
-        res.json(return_data);
-      } else {
-        res.status(400).json({
-          status: "error",
-          error: "Authentication error!",
+            if (password_check) {
+
+                let token_data = {
+                    userType: 'organization',
+                    id: user._id,
+                    registrationNo
+                };
+
+                let accessToken = auth.createAccessToken(token_data);
+                let refreshToken = auth.createRefreshToken(token_data);
+
+                await orgDBHelper.saveRefreshToken(refreshToken, user._id);
+
+                res.header("x-access-token", accessToken);
+                res.header("x-refresh-token", refreshToken);
+
+                let name = user.name;
+
+                return_data = {
+                    status: "ok",
+                    userType: "organization",
+                    data: {
+                        registrationNo: registrationNo,
+                        id: user._id,
+                        name: name,
+                    },
+                };
+                res.json(return_data);
+            } else {
+                res.status(400).json({
+                    status: "error",
+                    error: "Authentication error!",
+                });
+            }
+        } else {
+            res.status(400).json({
+                status: "error",
+                error: "Authentication error!",
+            });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            status: "error",
+            error: "Internal server error!",
         });
-      }
-    } else {
-      res.status(400).json({
-        status: "error",
-        error: "Authentication error!",
-      });
     }
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      status: "error",
-      error: "Internal server error!",
-    });
-  }
 };
 
 //post station login details
 const login_post_station = async (req, res) => {
-  const registrationNo = req.body.registrationNo;
-  const password = req.body.password;
+    
+    const registrationNo = req.body.registrationNo;
+    const password = req.body.password;
 
-  try {
-    const user = await stationDBHelper.findStationByRegNo(registrationNo);
+    try {
+        const user = await stationDBHelper.findStationByRegNo(registrationNo);
 
-    if (user !== null) {
-      let password_check = await encHandler.checkEncryptedCredential(
-        password,
-        user.password
-      );
+        if (user !== null) {
+            let password_check = await encHandler.checkEncryptedCredential(
+                password,
+                user.password
+            );
 
-      if (password_check) {
-        let token = auth.createToken();
-        let name = user.name;
+            if (password_check) {
 
-        return_data = {
-          status: "ok",
-          token: token,
-          userType: "station",
-          data: {
-            registrationNo: registrationNo,
-            id: user._id,
-            name: name,
-          },
-        };
-        res.json(return_data);
-      } else {
-        res.status(400).json({
-          status: "error",
-          error: "Authentication error!",
+                let token_data = {
+                    userType: 'station',
+                    id: user._id,
+                    registrationNo
+                };
+
+                let accessToken = auth.createAccessToken(token_data);
+                let refreshToken = auth.createRefreshToken(token_data);
+
+                await stationDBHelper.saveRefreshToken(refreshToken);
+
+                res.header("x-access-token", accessToken);
+                res.header("x-refresh-token", refreshToken);
+
+                let name = user.name;
+
+                return_data = {
+                    status: "ok",
+                    userType: "station",
+                    data: {
+                        registrationNo: registrationNo,
+                        id: user._id,
+                        name: name,
+                    },
+                };
+                res.json(return_data);
+            } else {
+                res.status(400).json({
+                    status: "error",
+                    error: "Authentication error!",
+                });
+            }
+        } else {
+            res.status(400).json({
+                status: "error",
+                error: "Authentication error!",
+            });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            status: "error",
+            error: "Internal server error!",
         });
-      }
-    } else {
-      res.status(400).json({
-        status: "error",
-        error: "Authentication error!",
-      });
     }
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      status: "error",
-      error: "Internal server error!",
-    });
-  }
 };
 
 module.exports = {
-  register_post_personal,
-  register_post_org,
-  login_post_admin,
-  login_post_personal,
-  login_post_org,
-  login_post_station,
+    register_post_personal,
+    register_post_org,
+    login_post_admin,
+    login_post_personal,
+    login_post_org,
+    login_post_station,
 };
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,60 +1,167 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-let requireAuth = (req, res, next) => {
+const personalDBHelper = require("../services/personalDBHelper");
+const orgDBHelper = require("../services/orgDBHelper");
+const stationDBHelper = require("../services/stationDBHelper");
 
-    let token;
+const ACCESS_TOKEN_SECRET_KEY = process.env.ACCESS_TOKEN_SECRET_KEY;
+const REFRESH_TOKEN_SECRET_KEY = process.env.REFRESH_TOKEN_SECRET_KEY
+const ACCESS_EXP = process.env.ACCESS_EXP;
+const REFRESH_EXP = process.env.REFRESH_EXP
+
+let requireAuth = async (req, res, next) => {
+
     try {
-        token = req.headers['token'].replace(/['"]+/g, '');
-    }
+
+        let refreshTokenHeader = req.headers["x-refresh-token"];
+        let accessTokenHeader = req.headers["x-access-token"];
+        // console.log(refreshTokenHeader);
+        // console.log(accessTokenHeader);
+        if (refreshTokenHeader === undefined && accessTokenHeader === undefined) {
+
+            return res.status(404).json({
+                status: 'auth-error',
+                error: 'auth token not found'
+            });
+        }
+        else {
+            // console.log("tokens defined")
+            let refreshToken = refreshTokenHeader && refreshTokenHeader.split(" ")[1]; //TODO:check whether bearer is there to split
+            let accessToken = accessTokenHeader && accessTokenHeader.split(" ")[1];
+            // console.log(refreshToken)
+            // console.log(accessToken)
+            if (accessToken) {
+                // console.log(accessToken)
+                const accessTokenCheck = accessTokenVerify(accessToken);
+                // console.log(accessTokenCheck)
+                if (accessTokenCheck.status === "ok") {
+
+                    res.header("x-access-token", accessTokenCheck.newAccessToken);
+                    req.userData = accessTokenCheck.data;
+
+                    return next();
+                }
+                else {
+
+                    const refreshTokenCheck = await refreshTokenVerify(refreshToken);
+
+                    if (refreshTokenCheck.status === "ok") {
+
+                        res.header("x-access-token", refreshTokenCheck.newAccessToken);
+                        req.userData = refreshTokenCheck.data;
+
+                        return next();
+                    }
+                    else {
+                        return res.status(401).json(refreshTokenCheck);
+                    }
+                }
+            }
+            else {
+                console.log("access token invalid")
+                const refreshTokenCheck = await refreshTokenVerify(refreshToken);
+
+                if (refreshTokenCheck.status === "ok") {
+
+                    res.header("x-access-token", refreshTokenCheck.newAccessToken);
+                    req.userData = refreshTokenCheck.data;
+
+                    return next();
+                }
+                else {
+                    return res.status(401).json(refreshTokenCheck);
+                }
+            }
+        }
+    } 
     catch (err) {
-        res.json({
-            status: 'auth-error',
-            error: "Your login session has expired, please re-login to proceed!"
+        res.status(500).send({
+            status: 'error',
+            error: 'Internal server error!'
         });
     }
-
-    //check whether jwt exists and it is verified
-    if(token){
-        jwt.verify(token, process.env.JWT_ENV, (err, decodedToken) => {
-            if(err){
-                console.log(err.message);
-                res.json({
-                    status: 'auth-error',
-                    error: "Your login session has expired, please re-login to proceed!"
-                });
-            }
-            else{
-                // req.decodedID = decodedToken;
-                next();
-            }
-        });
-    }
-    else{
-        console.log('not token');
-        res.json({
-            status: 'auth-error',
-            error: "Your login session has expired, please re-login to proceed!"
-        });
-    }
-
 };
 
-const maxAge = 1 * 24 * 60 * 60;
-const createToken = () => {
-    return jwt.sign({value: Date.now()}, process.env.JWT_ENV, {
-        expiresIn: maxAge
+const accessTokenVerify = (token) => {
+
+    try {
+
+        const value = jwt.verify(token, ACCESS_TOKEN_SECRET_KEY);
+        const newAccessToken = jwt.sign(value, ACCESS_TOKEN_SECRET_KEY, { expiresIn: ACCESS_EXP });
+
+        return { status: "ok", data: value, newAccessToken: newAccessToken };
+    }
+    catch (error) {
+        // console.log(error)
+        return { status: "error", error: "token expired!" };
+    }
+};
+
+
+const refreshTokenVerify = async (token) => {
+
+    try {
+
+        const value = jwt.verify(token, REFRESH_TOKEN_SECRET_KEY);
+
+        delete value.iat;
+        delete value.exp;
+
+        let user;
+        if (value.userType === 'admin' && value.username === process.env.ADMIN_USERNAME) {
+
+            const newAccessToken = jwt.sign(value, ACCESS_TOKEN_SECRET_KEY, { expiresIn: ACCESS_EXP });
+            return { status: "ok", data: value, newAccessToken: newAccessToken };
+        }
+        else if (value.userType === 'personal') {
+            user = await personalDBHelper.findClientByID(value.id);
+        }
+        else if (value.userType === 'organization') {
+            user = await orgDBHelper.findClientByID(value.id)
+        }
+        else if (value.userType === 'station') {
+            user = await stationDBHelper.findStationByID(value.id);
+        }
+
+        if (user) {
+
+            if (value.id === user.id) {
+
+                const newAccessToken = jwt.sign(value, ACCESS_TOKEN_SECRET_KEY, { expiresIn: ACCESS_EXP });
+
+                return { status: "ok", data: value, newAccessToken: newAccessToken };
+            }
+            else {
+
+                return { status: "error", error: "Invalid token!" };
+            }
+        }
+        else {
+
+            return { status: "error", error: "Invalid token!" };
+        }
+    }
+    catch (error) {
+        console.log(error)
+        return { status: "error", error: "Invalid/expired token!" };
+    }
+};
+
+const createAccessToken = (data) => {
+    return jwt.sign(data, ACCESS_TOKEN_SECRET_KEY, {
+        expiresIn: ACCESS_EXP
     });
 };
 
-const createTokenWithUserID = (userID) => {
-    return jwt.sign({userID}, process.env.JWT_ENV, {
-        expiresIn: maxAge
+const createRefreshToken = (data) => {
+    return jwt.sign(data, REFRESH_TOKEN_SECRET_KEY, {
+        expiresIn: REFRESH_EXP
     });
 };
 
 module.exports = {
     requireAuth,
-    createToken,
-    createTokenWithUserID,
+    createAccessToken,
+    createRefreshToken,
 };
