7b14ece7876931ca080192258aa3033b2b0e01ce
Kalana99 <kalanarajika99@gmail.com>
2022-10-14 23:34:03 +0530

    new auth tests

--- tests/unit/middlewares/auth.test.js
+++ tests/unit/middlewares/auth.test.js
@@ -1,7 +1,12 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const { requireAuth, createToken } = require('../../../middleware/auth');
+const { requireAuth, accessTokenVerify, refreshTokenVerify, createAccessToken, createRefreshToken } = require('../../../middleware/auth');
+
+const ACCESS_TOKEN_SECRET_KEY = process.env.ACCESS_TOKEN_SECRET_KEY;
+const REFRESH_TOKEN_SECRET_KEY = process.env.REFRESH_TOKEN_SECRET_KEY;
+const ACCESS_EXP = process.env.ACCESS_EXP;
+const REFRESH_EXP = process.env.REFRESH_EXP;
 
 describe("auth middleware check", () => {
 
@@ -30,19 +35,145 @@ describe("auth middleware check", () => {
     //     });
     // });
 
-    describe("createToken - return an encrypted token containing the created time", () => {
+    describe("accessTokenVerify - verifies the validity of a given access token", () => {
 
-        it("the return token should contain the today date", async () => {
+        it("should return an error for an invalid token", async () => {
 
-            let date_obj = new Date(Date.now());
+            let invalid_token = "hdcwevuwveuvwueuwevyu7233vf23fgi2ufi2f";
 
-            const token = createToken();
+            const result = accessTokenVerify(invalid_token);
 
-            jwt.verify(token, process.env.JWT_ENV, (err, decodedToken) => {
+            expect(result.status).toEqual("error");
+        });
 
-                let expected_date_obj = new Date(decodedToken.value);
+        it("should return a 'ok' status and a new access token for a valid token", async () => {
 
-                expect(expected_date_obj).toEqual(date_obj);
+            let token_value = {id: "some_id"};
+            let valid_token = jwt.sign(token_value, ACCESS_TOKEN_SECRET_KEY, { expiresIn: ACCESS_EXP });;
+
+            const result = accessTokenVerify(valid_token);
+
+            expect(result.status).toEqual("ok");
+            expect(result.hasOwnProperty('newAccessToken')).toBeTruthy();
+        });
+    });
+
+    describe("refreshTokenVerify - verifies the validity of a given refresh token", () => {
+
+        it("should return an error for an invalid token", async () => {
+
+            let invalid_token = "hdcwevuwveuvwueuwevyu7233vf23fgi2ufi2f";
+
+            const result = await refreshTokenVerify(invalid_token);
+
+            expect(result.status).toEqual("error");
+            expect(result.error).toEqual("Invalid/expired token!");
+        });
+
+        it("if the userType is provided as admin with a wrong username, should return an error", async () => {
+
+            let token_value = {
+                userType: "admin",
+                useranme: "something"
+            };
+            let valid_token = jwt.sign(token_value, REFRESH_TOKEN_SECRET_KEY, { expiresIn: REFRESH_EXP });;
+
+            const result = await refreshTokenVerify(valid_token);
+
+            expect(result.status).toEqual("error");
+            expect(result.error).toEqual("Invalid token!");
+        });
+
+        it("if the userType is provided as admin with the correct admin username, should return a 'ok' status with a new access token", async () => {
+
+            let token_value = {
+                userType: "admin",
+                username: process.env.ADMIN_USERNAME
+            };
+            let valid_token = jwt.sign(token_value, REFRESH_TOKEN_SECRET_KEY, { expiresIn: REFRESH_EXP });;
+
+            const result = await refreshTokenVerify(valid_token);
+
+            expect(result.status).toEqual("ok");
+            expect(result.hasOwnProperty('newAccessToken')).toBeTruthy();
+        });
+
+        it("if the userType is non-existing, should return an error", async () => {
+
+            let token_value = {
+                userType: "something"
+            };
+            let valid_token = jwt.sign(token_value, REFRESH_TOKEN_SECRET_KEY, { expiresIn: REFRESH_EXP });;
+
+            const result = await refreshTokenVerify(valid_token);
+
+            expect(result.status).toEqual("error");
+            expect(result.error).toEqual("Invalid token!");
+        });
+
+        // it("if the id is non-existing for any userType(except admin), should return an error", async () => {
+
+        //     let userTypes = ["personal", "organization", "station"];
+        //     let token_value = {
+        //         // userType: userTypes[Math.floor(Math.random() * 3 )],
+        //         userType: "personal",
+        //         id: "633ed6babc0b58545e91c3ee"
+        //     };
+        //     let valid_token = jwt.sign(token_value, REFRESH_TOKEN_SECRET_KEY, { expiresIn: REFRESH_EXP });;
+
+        //     const result = await refreshTokenVerify(valid_token);
+        //     // console.log(result)
+        //     expect(result.status).toEqual("error");
+        //     expect(result.error).toEqual("Invalid token!");
+        // });
+
+        // it("for an existing id, should return the status 'ok' with a new access token", async () => {
+
+        //     let userTypes = ["personal", "organization", "station"];
+        //     let token_value = {
+        //         // userType: userTypes[Math.floor(Math.random() * 3 )],
+        //         userType: "personal",
+        //         id: "633ed6babc0b58545e91c3ee"
+        //     };
+        //     let valid_token = jwt.sign(token_value, REFRESH_TOKEN_SECRET_KEY, { expiresIn: REFRESH_EXP });;
+
+        //     const result = await refreshTokenVerify(valid_token);
+
+        //     expect(result.status).toEqual("ok");
+        //     expect(result.hasOwnProperty('newAccessToken')).toBeTruthy();
+        // });
+    });
+
+    describe("createAccessToken - returns an encrypted access token containing the provided data", () => {
+
+        it("the return token should contain the provided data", async () => {
+
+            let data = {id: "some_id"};
+
+            const token = createAccessToken(data);
+
+            jwt.verify(token, ACCESS_TOKEN_SECRET_KEY, (err, decodedToken) => {
+
+                let expected_data = decodedToken;
+
+                expect(expected_data.id).toEqual(data.id);
+            });
+        });
+    });
+
+    describe("createRefreshToken - returns an encrypted refresh token containing the provided data", () => {
+
+        it("the return token should contain the provided data", async () => {
+
+            let data = {id: "some_id"};
+
+            const token = createRefreshToken(data);
+
+            jwt.verify(token, REFRESH_TOKEN_SECRET_KEY, (err, decodedToken) => {
+
+                let expected_data = decodedToken;
+
+                expect(expected_data.id).toEqual(data.id);
             });
         });
     });
@@ -51,5 +182,4 @@ describe("auth middleware check", () => {
 //remaining
 
 // requireAuth,
-// createAccessToken,
-// createRefreshToken,
+// refreshTokenVerify
