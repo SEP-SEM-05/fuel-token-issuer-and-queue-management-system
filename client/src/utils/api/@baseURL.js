26b8e901b816240169677a8509ee0fe74ba71c8f
Kalana99 <kalanarajika99@gmail.com>
2022-10-31 02:14:29 +0530

    authEndpointTesting init

--- client/src/utils/api/@baseURL.js
+++ client/src/utils/api/@baseURL.js
@@ -5,7 +5,13 @@ let baseApi = "/";
 const refreshToken = localStorage.getItem("refreshToken");
 const accessToken = sessionStorage.getItem("accessToken");
 
+// console.log(refreshToken)
+// console.log(accessToken)
+
 if (url) {
+
+    // const refreshToken = localStorage.getItem("refreshToken");
+    // const accessToken = sessionStorage.getItem("accessToken");
 
     baseApi = axios.create({
         baseURL: url,
@@ -14,6 +20,14 @@ if (url) {
             "x-access-token": accessToken ? "Bearer " + accessToken : undefined,
         },
     });
+
+    // baseApi = axios.create({
+    //     baseURL: url,
+    //     headers: {
+    //         "x-refresh-token": localStorage.getItem("refreshToken") ? "Bearer " + localStorage.getItem("refreshToken") : undefined,
+    //         "x-access-token": sessionStorage.getItem("accessToken") ? "Bearer " + sessionStorage.getItem("accessToken") : undefined,
+    //     },
+    // });
 }
 
 export default baseApi;
