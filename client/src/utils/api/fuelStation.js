e8f05d18c2fb844813b69df50800c3d99e99c545
Kalana99 <kalanarajika99@gmail.com>
2022-11-05 18:07:18 +0530

    org navbar set

--- client/src/utils/api/fuelStation.js
+++ client/src/utils/api/fuelStation.js
@@ -4,32 +4,32 @@ import axios from "axios";
 
 //fuel station login
 const signIn = async (data) => {
-  try {
-    
-    let response = await baseApi.post(
-      "auth/loginStation",
-      data
-    );
-            localStorage.setItem(
-              "refreshToken",
-              response.headers["x-refresh-token"]
-            );
-            sessionStorage.setItem(
-              "accessToken",
-              response.headers["x-access-token"]
-            );
-    return response.data;
-  } catch (err) {
-    console.log(err);
-    return err.response.data;
-  }
+    try {
+
+        let response = await baseApi.post(
+            "auth/loginStation",
+            data
+        );
+        localStorage.setItem(
+            "refreshToken",
+            response.headers["x-refresh-token"]
+        );
+        sessionStorage.setItem(
+            "accessToken",
+            response.headers["x-access-token"]
+        );
+        return response.data;
+    } catch (err) {
+        console.log(err);
+        return err.response.data;
+    }
 };
 
 //get dashboard info
 const getDashBoard = async (id) => {
 
     try {
-        
+
         const refreshToken = localStorage.getItem("refreshToken");
         const accessToken = sessionStorage.getItem("accessToken");
 
@@ -45,11 +45,11 @@ const getDashBoard = async (id) => {
             `station/dashboard/${id}`
         )
 
-        if(response.headers["x-access-token"]){
+        if (response.headers["x-access-token"]) {
             sessionStorage.setItem("accessToken", response.headers["x-access-token"]);
         }
         return response.data;
-    } 
+    }
     catch (err) {
         return err.response.data;
     }
@@ -57,34 +57,34 @@ const getDashBoard = async (id) => {
 
 // add new fuel amount
 const addFuelAmount = async (data) => {
-  try {
-    let response = await baseApi.post(`station/updateamount`, data);
-  
-    if (response.headers["x-access-token"]) {
-      sessionStorage.setItem("accessToken", response.headers["x-access-token"]);
+    try {
+        let response = await baseApi.post(`station/updateamount`, data);
+
+        if (response.headers["x-access-token"]) {
+            sessionStorage.setItem("accessToken", response.headers["x-access-token"]);
+        }
+
+        return response.data;
+    } catch (err) {
+        console.log(err);
+        return err.response.data;
     }
-    
-    return response.data;
-  } catch (err) {
-    console.log(err);
-    return err.response.data;
-  }
 }
 
 //get waiting queues
 const getWaitingQueues = async (id) => {
-  try {
-    let response = await baseApi.get(`station/fuelqueues/${id}`);
-    //console.log(response.data);
+    try {
+        let response = await baseApi.get(`station/fuelqueues/${id}`);
+        //console.log(response.data);
 
-    if (response.headers["x-access-token"]) {
-      sessionStorage.setItem("accessToken", response.headers["x-access-token"]);
+        if (response.headers["x-access-token"]) {
+            sessionStorage.setItem("accessToken", response.headers["x-access-token"]);
+        }
+
+        return response.data;
+    } catch (err) {
+        return err.response.data;
     }
-
-    return response.data;
-  } catch (err) {
-    return err.response.data;
-  }
 }
 
 export { signIn, getDashBoard, getWaitingQueues, addFuelAmount };
